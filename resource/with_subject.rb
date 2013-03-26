2d6e4c10d583c927713425aa677b2aad2693497b
Robert Dallas Gray <mail@robertdallasgray.com>
2013-03-26 20:45:22 +0000

    Removed validations for parent in Resource::WithSubject.

--- resource/with_subject.rb
+++ resource/with_subject.rb
@@ -4,23 +4,7 @@ module Bbx
 
       def self.extended(base)
         base.class_eval do
-          include InstanceMethods
           belongs_to :subject, :polymorphic => true
-          validates :subject_type, :presence => true
-          validates :subject_id,   :presence => true
-          validate  :subject_exists
-        end
-      end
-
-      module InstanceMethods
-
-        private
-
-        def subject_exists
-          subject_class = Kernel.const_get(self.subject_type)
-          subject = subject_class.find(self.subject_id)
-        rescue
-          errors.add(:subject_id, "was not found")
         end
       end
     end
